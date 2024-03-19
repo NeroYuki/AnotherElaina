7af5aa4eda7f7c1c581f658176a53d84269682a5
NeroYuki <neroyuki241@gmail.com>
2024-03-19 10:42:41 +0700

    add security reminder

--- utils/load_discord_img.js
+++ utils/load_discord_img.js
@@ -2,6 +2,7 @@ const { default: axios } = require('axios');
 
 function loadImage(url, getBuffer = false) {
     // download image from the given url and convert them to base64 dataURI (with proper mime type) or buffer use axios
+    // possible security issue with arbitrary file download, need to check for magic header that match the mime type
 
     return new Promise((resolve, reject) => {
         axios.get(url, { responseType: 'arraybuffer' })
@@ -12,6 +13,9 @@ function loadImage(url, getBuffer = false) {
                 }
                 let image = Buffer.from(response.data, 'binary').toString('base64');
                 let mime = response.headers['content-type'];
+
+                // TODO: check for magic header
+
                 resolve(`data:${mime};base64,${image}`);
             })
             .catch((error) => {
