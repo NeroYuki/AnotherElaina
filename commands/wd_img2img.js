3b3368d7bb6891403357deca3d958ae698307abd
NeroYuki <neroyuki241@gmail.com>
2024-08-18 14:30:50 +0700

    bug fix

--- commands/wd_img2img.js
+++ commands/wd_img2img.js
@@ -312,7 +312,7 @@ currently cached models: ${cached_model.map(x => check_model_filename(x)).join('
             seed, sampler, session_hash, height, width, attachment, null, denoising_strength, /*img2img mode*/ 0, 4, "original", upscaler, 
             false, extra_config.coupler_config, extra_config.color_grading_config, clip_skip, is_censor,
             extra_config.freeu_config, extra_config.dynamic_threshold_config, extra_config.pag_config, "Whole picture", 32, 
-            override_neg_prompt ? false : true, extra_config.use_booru_gen, booru_gen_config)
+            override_neg_prompt ? false : true, extra_config.use_booru_gen, null)
 
         // make option_init but for axios
         const option_init_axios = {
