b1d968dd6d210d978623da3bf1c5d7449b68fa5d
NeroYuki <neroyuki241@gmail.com>
2025-01-09 02:38:18 +0700

    more fix

--- commands/operating/server_stat.js
+++ commands/operating/server_stat.js
@@ -3,6 +3,7 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const { packages } = require('../../package-lock.json');
 const ComfyClient = require('../../utils/comfy_client');
 const si = require('systeminformation');
+const os = require('os');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,21 +16,6 @@ module.exports = {
 		// fetch the server stat
         const server_stat = ComfyClient.comfyStat;
 
-        const cpuStat = await si.cpu().catch((err) => {
-            console.log("Failed to retrieve cpu stat", err)
-            return
-        })
-
-        const cpuCurrentStat = await si.cpuCurrentSpeed().catch((err) => {
-            console.log("Failed to retrieve cpu current stat", err)
-            return
-        })
-
-        const cpuTemp = await si.cpuTemperature().catch((err) => {
-            console.log("Failed to retrieve cpu temperature", err)
-            return
-        })
-
         const memStat = await si.mem().catch((err) => {
             console.log("Failed to retrieve memory stat", err)
             return
@@ -40,15 +26,36 @@ module.exports = {
             return
         })
 
+        const cpuTemp = await si.cpuTemperature().catch((err) => {
+            console.log("Failed to retrieve cpu temp", err)
+            return
+        })
+
         const ram_percent = (memStat?.used && memStat?.total) ? memStat.used / memStat.total * 100 : 0
         const ram_used = memStat?.used ? memStat.used / 1024 / 1024 / 1024 : 0
         const ram_total = memStat?.total ? memStat.total / 1024 / 1024 / 1024 : 0
 
-        const cpu_percent = (cpuCurrentStat && cpuStat) ? cpuCurrentStat.avg * 100 / cpuStat.speedMax : 0
+        // get the main cpu usage percentage from os.cpus()
+        let cpus = os.cpus();
+
+        let cpu_main_percent = 0;
+        for(let i = 0, len = cpus.length; i < len; i++) {
+            var cpu = cpus[i], total = 0;
+
+            for(var type in cpu.times) {
+                total += cpu.times[type];
+            }
+
+            for(type in cpu.times) {
+                cpu_main_percent = Math.round(100 * cpu.times[type] / total)
+            }
+        }
+
+        console.log(cpuTemp)
 
         const botServerTitle = "Bot Server - " + `${osInfo.distro} ${osInfo.release} (${osInfo.arch})`
         const botServerStat = '------------------------------------------' + "\n"
-            + '**CPU Usage: **' + cpu_percent.toFixed(2) + "%" + "\n"
+            + '**CPU Usage: **' + cpu_main_percent.toFixed(2) + "%" + "\n"
             + (osInfo.distro.includes('Windows') ? '' : '**CPU Temp: **' + cpuTemp.main + "°C" + "\n")
             + '**RAM Usage: **' + `${ram_used.toFixed(2)}/${ram_total.toFixed(2)}GB (${ram_percent.toFixed(2)}%)` + "\n"
 
