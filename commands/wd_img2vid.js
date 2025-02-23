6c29652e12c7fa54995210c8b8e4dfc9ee5f2d1a
NeroYuki <neroyuki241@gmail.com>
2025-02-23 09:05:52 +0700

    add support for img2vid fuctionality

---
+++ commands/wd_img2vid.js
@@ -0,0 +1,144 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const og_workflow = require('../resources/skyreel_img2vid.json')
+const ComfyClient = require('../utils/comfy_client');
+const { get_teacache_config_from_prompt } = require('../utils/prompt_analyzer');
+const { clamp } = require('../utils/common_helper');
+
+const preset_to_budget_allocation = {
+    'fast': 10_000_000,
+    'std': 12_000_000,
+    'high': 16_000_000,
+    'extreme': 20_000_000
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wd_img2vid')
+            .setDescription('Generate a video from an image and a text prompt')
+            .addAttachmentOption(option =>
+                option.setName('image')
+                    .setDescription('The image to be converted')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('prompt')
+                    .setDescription('The text prompt to generate the video')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('neg_prompt')
+                    .setDescription('The negative text prompt to generate the video')
+                    .setRequired(false))
+            .addStringOption(option => 
+                option.setName('preset')
+                    .setDescription('The preset to use for the generation')
+                    .addChoices(
+                        { name: 'Fast (10M pixels)', value: 'fast' },
+                        { name: 'Standard (12M pixels)', value: 'std' },
+                        { name: 'High (16M pixels)', value: 'high' },
+                        { name: 'Extreme (20M pixels)', value: 'extreme' },
+                        { name: 'Custom', value: 'custom' }
+                    ))
+            .addIntegerOption(option =>
+                option.setName('custom_budget')
+                    .setDescription('Number of pixels allocate to the generation (max 24 million pixels)')
+                    .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('length')
+                    .setDescription('The length of the video in seconds (max 5 seconds, deafult 3)')
+                    .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('fps')
+                    .setDescription('The frame rate of the video (default 16, recommend max 24)')
+                    .setRequired(false))
+    ,
+
+	async execute(interaction, client) {
+        await interaction.deferReply();
+
+        let prompt = interaction.options.getString('prompt');
+        let neg_prompt = interaction.options.getString('neg_prompt') || '';
+        const attachment_option = interaction.options.getAttachment('image');
+        const preset = interaction.options.getString('preset') || 'std';
+        const length = clamp(interaction.options.getInteger('length') || 3, 1, 5);
+        const fps = clamp(interaction.options.getInteger('fps') || 16, 5, 24);
+        const custom_budget = clamp(interaction.options.getInteger('custom_budget') || 12_000_000, 300_000, 24_000_000);
+
+        let workflow = JSON.parse(JSON.stringify(og_workflow))
+
+        const forceHighPrecision = prompt.includes("[FORCE_Q6]")
+        if (forceHighPrecision) {
+            prompt = prompt.replace("[FORCE_Q6]", "")
+            workflow["120"]["inputs"]["unet_name"] = "skyreels-hunyuan-I2V-Q6_K.gguf"
+        }
+
+        if (image_info == null) {
+            interaction.editReply({ content: "Failed to receive input image" });
+            return
+        }
+
+        //download the image from attachment.proxyURL
+        let attachment = await loadImage(attachment_option.proxyURL,
+            /*getBuffer:*/ true).catch((err) => {
+            console.log("Failed to retrieve image from discord", err)
+            return
+        })
+        
+        //set the image buffer to the workflow
+        const image_info = await ComfyClient.uploadImage(attachment, Date.now() + "_" + attachment_option.name, attachment_option.contentType).catch((err) => {
+            console.log("Failed to upload image", err)
+            return
+        })
+
+        console.log(image_info)
+
+        // calculate the allocated bugdet to frame count and resolution
+        const frame_count = length * fps
+        const budget = preset_to_budget_allocation[preset] 
+        if (preset === 'custom') {
+            budget = custom_budget
+        }
+
+        workflow["42"]["inputs"]["noise_seed"] = Math.floor(Math.random() * 2_000_000_000)
+        workflow["110"]["inputs"]["seed"] = Math.floor(Math.random() * 2_000_000_000)
+        workflow["151"]["inputs"]["image"] = image_info.name
+        workflow["140"]["inputs"]["float"] = budget
+        workflow["153"]["inputs"]["int"] = frame_count
+        workflow["50"]["inputs"]["frame_rate"] = fps
+
+        workflow["45"]["inputs"]["text"] = "FPS-24, " + prompt
+        workflow["85"]["inputs"]["text"] = "FPS-24, " + neg_prompt
+
+        ComfyClient.sendPrompt(workflow, (data) => {
+            if (data.node !== null) interaction.editReply({ content: "Processing: " + workflow[data.node]["_meta"]["title"] });
+        }, (data) => {
+            console.log('received success')
+            const filename = data.output.gifs[0].filename
+
+            // fetch video from comfyUI
+            ComfyClient.getImage(filename, '', '', /*only_filename*/ true).then(async (arraybuffer) => {
+                // convert arraybuffer to buffer
+                const buffer = Buffer.from(arraybuffer)
+
+                await interaction.editReply({ content: "Generation Success", files: [{ attachment: buffer, name: filename }] });
+            }).catch((err) => {
+                console.log("Failed to retrieve video", err)
+                interaction.editReply({ content: "Failed to retrieve video" });
+            }).finally(() => {
+                ComfyClient.freeMemory(true)
+            })
+
+        }, (data) => {
+            console.log('received error')
+            interaction.editReply({ content: data.error });
+            ComfyClient.freeMemory(true)
+        }, (data) => {
+            console.log('received progress')
+
+            // skip video combine node update progress (too spammy)
+            if (workflow[data.node]["_meta"]["title"] === "Video Combine 🎥🅥🅗🅢") return
+
+            interaction.editReply({ content: "Processing: " + workflow[data.node]["_meta"]["title"] + ` (${data.value}/${data.max})` });
+        });
+
+        // await interaction.editReply('This command is not implemented yet');
+	},
+};
--- commands/wd_txt2vid.js
+++ commands/wd_txt2vid.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const workflow = require('../resources/comfy_txt2vid.json')
+const og_workflow = require('../resources/comfy_txt2vid.json')
 const ComfyClient = require('../utils/comfy_client');
 const { get_teacache_config_from_prompt } = require('../utils/prompt_analyzer');
+const { clamp } = require('../utils/common_helper');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -18,10 +19,27 @@ module.exports = {
                         { name: '512x320 - 3 secs', value: 'fast' },
                         { name: '640x480 - 3 secs', value: 'sd' },
                         { name: '512x320 - 5 secs', value: 'long' },
+                        { name: 'Custom', value: 'custom' }
                     ))
+            .addIntegerOption(option =>
+                option.setName('custom_width')
+                    .setDescription('The width of the video in pixels (max 1280, default 512)')
+                    .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('custom_height')
+                    .setDescription('The height of the video in pixels (max 1280, default 320)')
+                    .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('custom_length')
+                    .setDescription('The length of the video in seconds (max 5 seconds, deafult 3)')
+                    .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('custom_fps')
+                    .setDescription('The frame rate of the video (default 16, recommend max 24)')
+                    .setRequired(false))
             .addBooleanOption(option =>
                 option.setName('portrait')
-                    .setDescription('Generate a portrait video')
+                    .setDescription('Generate a portrait video, no effect on custom preset')
                     .setRequired(false))
             .addBooleanOption(option =>
                 option.setName('no_optimize')
@@ -41,30 +59,56 @@ module.exports = {
 	async execute(interaction, client) {
         let prompt = interaction.options.getString('prompt');
         const preset = interaction.options.getString('preset') || 'fast';
+        const custom_width = clamp(interaction.options.getInteger('custom_width') || 512, 64, 1280);
+        const custom_height = clamp(interaction.options.getInteger('custom_height') || 320, 64, 1280);
+        const custom_length = clamp(interaction.options.getInteger('custom_length') || 3, 1, 5);
+        const custom_fps = clamp(interaction.options.getInteger('custom_fps') || 16, 5, 24);
         const portrait = interaction.options.getBoolean('portrait') || false;
         const no_optimize = interaction.options.getBoolean('no_optimize') || false;
         const do_upscale = interaction.options.getBoolean('do_upscale') || false;
         const do_frame_interpolation = interaction.options.getBoolean('do_frame_interpolation') || false;
 
-        workflow["15"]["inputs"]["text"] = prompt
+        let workflow = JSON.parse(JSON.stringify(og_workflow))
+
+        const forceHighPrecision = prompt.includes("[FORCE_Q6]")
+        if (forceHighPrecision) {
+            prompt = prompt.replace("[FORCE_Q6]", "")
+            workflow["11"]["inputs"]["unet_name"] = "hunyuan-video-t2v-720p-Q6_K.gguf"
+        }
 
         if (preset == 'fast') {
             workflow["12"]["inputs"]["width"] = 512
             workflow["12"]["inputs"]["height"] = 320
             workflow["12"]["inputs"]["length"] = 49
         }
-        if (preset == 'sd') {
+        else if (preset == 'sd') {
             workflow["12"]["inputs"]["width"] = 640
             workflow["12"]["inputs"]["height"] = 480
             workflow["12"]["inputs"]["length"] = 49
         }
-        if (preset == 'long') {
+        else if (preset == 'long') {
             workflow["12"]["inputs"]["width"] = 512
             workflow["12"]["inputs"]["height"] = 320
             workflow["12"]["inputs"]["length"] = 81
         }
+        else {
+            // calculate budget
+            const budget = custom_width * custom_height * custom_fps * custom_length
+            if (budget > 24_000_000) {
+                interaction.editReply({ content: "Budget exceeded 24M pixels! My PC cannot handle this xd" });
+                return
+            }
+            else if (budget > 16_000_000) {
+                interaction.channel.send({ content: "Budget exceeded 16M pixels, generation may take quite a while" });
+            }
 
-        if (portrait) {
+            workflow["12"]["inputs"]["width"] = custom_width
+            workflow["12"]["inputs"]["height"] = custom_height
+            workflow["12"]["inputs"]["length"] = custom_length * custom_fps
+            workflow["41"]["inputs"]["frame_rate"] = custom_fps
+        }
+
+        if (portrait && preset != 'custom') {
             // swap width and height
             var a = workflow["12"]["inputs"]["width"]
             workflow["12"]["inputs"]["width"] = workflow["12"]["inputs"]["height"]
@@ -83,8 +127,9 @@ module.exports = {
             workflow["46"]["inputs"]["frames"] = ["37", 0]
         }
 
-        if (!do_frame_interpolation) {
-            workflow["47"]["inputs"]["Value"] = 1
+        if (do_frame_interpolation) {
+            workflow["47"]["inputs"]["Value"] = 2
+            workflow["41"]["inputs"]["frame_rate"] = preset == 'custom' ? custom_fps * 2 : 32
         }
 
         await interaction.deferReply();
@@ -95,6 +140,8 @@ module.exports = {
             workflow["45"]["inputs"]["rel_l1_thresh"] = teacache_check.teacache_config.threshold
         }
         prompt = teacache_check.prompt
+
+        workflow["15"]["inputs"]["text"] = prompt
 
         ComfyClient.sendPrompt(workflow, (data) => {
             if (data.node !== null) interaction.editReply({ content: "Processing: " + workflow[data.node]["_meta"]["title"] });
