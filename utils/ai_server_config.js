5142b116733be8d476d6199cd17f80bf84c66e23
NeroYuki <neroyuki241@gmail.com>
2025-03-22 23:51:59 +0700

    update model list

--- utils/ai_server_config.js
+++ utils/ai_server_config.js
@@ -1500,6 +1500,8 @@ const model_selection_legacy = [
     { name: 'IllusionBreed v3', value: 'illusionbreed_vpred_v30.safetensors'},
     { name: 'AnimagineXL v3', value: 'animaginexl_v3.safetensors'},
     { name: 'NekorayXL v0.6', value: 'nekorayxl.safetensors' },
+    { name: 'IllustriousXL v1', value: 'Illustriousxl_v10.safetensors'},
+    { name: 'WAI-NSFW-IllustriousXL v10', value: 'wai_nsfw_illustrious_v100.safetensors'},
     { name: 'SilenceMix v1', value: 'silencemix_v10.safetensors'},
     { name: 'Flux.dev Q4_K_S', value: 'flux1-dev-Q4_K_S.gguf' },
 ]
@@ -1527,7 +1529,9 @@ const model_selection_xl = [
     { name: 'ZukiCuteIL v5', value: 'zukicuteil_v50.safetensors'},
     { name: 'ArtiWaifu v2', value: 'artiwaifu_v2.safetensors'},
     { name: 'IllustriousXL v1', value: 'Illustriousxl_v10.safetensors'},
+    { name: 'IllustriousXL v1.1', value: 'Illustriousxl_v11.safetensors'},
     { name: 'WAI-NSFW-IllustriousXL v10', value: 'wai_nsfw_illustrious_v100.safetensors'},
+    { name: 'WAI-NSFW-IllustriousXL v12', value: 'wai_nsfw_illustrious_v120.safetensors'},
     { name: 'CatTower NoobAIXL V-pred v1.5' , value: 'cattowernoobaixl_vpred_v15.safetensors'},
 ]
 
