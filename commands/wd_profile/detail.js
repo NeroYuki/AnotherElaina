54ffb5dbea7b5eb8cb1109f095e56a0a40888a30
NeroYuki <neroyuki241@gmail.com>
2025-06-03 17:06:17 +0700

    add fetching wildcard via ai server

--- commands/wd_profile/detail.js
+++ commands/wd_profile/detail.js
@@ -2,6 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { queryRecordLimit } = require('../../database/database_interaction');
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const { truncate } = require('../../utils/common_helper');
+const { server_pool, get_worker_server } = require('../../utils/ai_server_config');
+const crypt = require('crypto');
+const { default: axios } = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,14 +17,105 @@ module.exports = {
         .addUserOption(option =>
             option.setName('user')
                 .setDescription('The user to return the profile of (Default to yourself)'))
+        .addBooleanOption(option =>
+            option.setName('server_wildcard')
+                .setDescription('Search for wildcard profile on the server, instead of user\'s profile')
+                .setRequired(false))
     ,
 
 	async execute(interaction) {
 		// parse the option
         const name = interaction.options.getString('name');
         let user = interaction.options.getUser('user') || interaction.user;
+        const server_wildcard = interaction.options.getBoolean('server_wildcard') || false;
 
         await interaction.deferReply();
+
+        let attachment = null;
+        let embeded = new MessageEmbed()
+            .setColor('#8888ff')
+            .setTitle('Profile Info')
+
+        if (server_wildcard) {
+            let server_index = get_worker_server(-1)
+
+            if (server_index === -1) {
+                await interaction.editReply({ content: "No server is available, please try again later"});
+                return
+            }
+
+            // TODO: add progress ping
+            const session_hash = crypt.randomBytes(16).toString('base64');
+
+            const WORKER_ENDPOINT = server_pool[server_index].url
+        
+            // sanitize the name
+            const sanitized_name = name.replace(/[^a-zA-Z0-9_\- ]/g, '_');
+            const fetch_list = [
+                "{\n  \"action\": \"load file\",\n  \"name\": \"" + sanitized_name + "\"}"
+            ]
+
+            const fn_index_fetch = server_pool[server_index].fn_fetch_wildcards
+
+            // make option_init but for axios
+            const option_init_axios = {
+                data: {
+                    fn_index: fn_index_fetch,
+                    session_hash: session_hash,
+                    data: fetch_list,
+                },
+                config: {
+                    timeout: 900000
+                }
+            }
+
+            try {
+                await axios.post(`${WORKER_ENDPOINT}/run/predict/`, option_init_axios.data, option_init_axios.config )
+                    .then((res) => {
+                        if (res.status !== 200) {
+                            throw 'Server can be reached but returned non-200 status'
+                        }
+                        return res.data
+                    }) // fuck node fetch, all my homies use axios
+                    .then(async (final_res_obj) => {
+                        const data = JSON.parse(final_res_obj.data[0])
+                        if (data.contents) {
+                            const prompt = `${data.contents || ''}`
+                            const is_too_big = prompt.length > 4000
+                            if (is_too_big) {
+                                attachment = new MessageAttachment(Buffer.from(prompt, 'utf-8'), 'prompt.txt');
+                            }
+                            // directly send the embeded here
+                            embeded.setDescription(`**Prompt:** ${is_too_big ? '<Too long to display, see attachment>' : prompt}`);
+                            embeded.setFooter({text: "Profile Owner: AI Server", iconURL: "https://cdn.discordapp.com/embed/avatars/0.png"});
+
+                            const reply_content = { embeds: [embeded] };
+                            if (attachment) {
+                                reply_content.files = [attachment];
+                            }
+                            
+                            await interaction.editReply(reply_content);
+                        }
+                        else {
+                            await interaction.editReply({content: `:x: Profile with name ${name} does not exist on the server`});
+                        }
+                    })
+                    .catch(err => {
+                        throw err
+                    });
+            }
+            catch (err) {
+                console.log(err)
+                try {
+                    await interaction.editReply({content: 'Error while fetching wildcard '})
+                }
+                catch (err) {
+                    console.log('cannot send error to discord', err)
+                }
+            }
+
+            return 
+        }
 
         // query the database
         let result = await queryRecordLimit('wd_profile', { name: name, user_id: user.id }, 1);
@@ -40,17 +134,10 @@ module.exports = {
         // compose the reply (only display non-default values)
         const data = result[0];
 
-        let attachment = null;
-
         if (user.id != result[0].user_id) {
             // fetch user info
             user = await interaction.client.users.fetch(result[0].user_id).catch(() => null);
         }
-
-        // send the reply
-        embeded = new MessageEmbed()
-            .setColor('#8888ff')
-            .setTitle('Profile Info')
 
         if (user) {
             embeded.setFooter({text: "Profile Owner: " + user.username, iconURL: user.avatarURL({dynamic: true}) || "https://cdn.discordapp.com/embed/avatars/0.png"});
