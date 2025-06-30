771b06460397babfc1421ae0cd2908e41e7adc53
NeroYuki <neroyuki241@gmail.com>
2025-06-30 21:33:33 +0700

    add wan to txt2vid and img2vid

--- commands/wd_img2vid.js
+++ commands/wd_img2vid.js
@@ -1,15 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const og_workflow = require('../resources/skyreel_img2vid.json')
+const wan_workflow = require('../resources/wan_img2vid.json')
 const ComfyClient = require('../utils/comfy_client');
 const { get_teacache_config_from_prompt } = require('../utils/prompt_analyzer');
 const { clamp } = require('../utils/common_helper');
 const { loadImage } = require('../utils/load_discord_img');
 
 const preset_to_budget_allocation = {
-    'fast': 10_000_000,
-    'std': 12_000_000,
-    'high': 16_000_000,
-    'extreme': 20_000_000
+    'fast': 20_000_000,
+    'std': 24_000_000,
+    'high': 32_000_000,
+    'extreme': 40_000_000
 }
 
 module.exports = {
@@ -32,10 +33,10 @@ module.exports = {
                 option.setName('preset')
                     .setDescription('The preset to use for the generation')
                     .addChoices(
-                        { name: 'Fast (10M pixels)', value: 'fast' },
-                        { name: 'Standard (12M pixels)', value: 'std' },
-                        { name: 'High (16M pixels)', value: 'high' },
-                        { name: 'Extreme (20M pixels)', value: 'extreme' },
+                        { name: 'Fast (20M pixels)', value: 'fast' },
+                        { name: 'Standard (24M pixels)', value: 'std' },
+                        { name: 'High (32M pixels)', value: 'high' },
+                        { name: 'Extreme (40M pixels)', value: 'extreme' },
                         { name: 'Custom', value: 'custom' }
                     ))
             .addIntegerOption(option =>
@@ -44,12 +45,25 @@ module.exports = {
                     .setRequired(false))
             .addIntegerOption(option =>
                 option.setName('length')
-                    .setDescription('The length of the video in seconds (max 5 seconds, deafult 3)')
+                    .setDescription('The length of the video in seconds (max 15 seconds, deafult 3)')
                     .setRequired(false))
             .addIntegerOption(option =>
                 option.setName('fps')
                     .setDescription('The frame rate of the video (default 16, recommend max 24)')
                     .setRequired(false))
+            .addIntegerOption(option =>
+                option.setName('shift')
+                    .setDescription('The shift rate for the video generation (default 3, higher shift = more motion)')
+                    .setRequired(false))
+            .addStringOption(option =>
+                option.setName('model')
+                    .setDescription('The model to use for the generation')
+                    .addChoices(
+                        { name: 'Skyreels Hunyuan I2V Q5 - Legacy', value: 'skyreels-hunyuan-I2V-Q5_K_M.gguf' },
+                        { name: 'Skyreels Wan v2 I2V Q6', value: 'Skywork-SkyReels-V2-I2V-14B-720P-Q6_K.gguf' },
+                        { name: 'Wan v2.1 I2V Q4_K_M', value: 'wan2.1-i2v-14b-720p-Q4_K_M.gguf' },
+                        { name: 'AniWan v2.1 I2V fp8', value: 'aniWan2114BFp8E4m3fn_i2v720p.safetensors' }
+                    ))
     ,
 
 	async execute(interaction, client) {
@@ -59,17 +73,13 @@ module.exports = {
         let neg_prompt = interaction.options.getString('neg_prompt') || '';
         const attachment_option = interaction.options.getAttachment('image');
         const preset = interaction.options.getString('preset') || 'std';
-        const length = clamp(interaction.options.getInteger('length') || 3, 1, 5);
+        const length = clamp(interaction.options.getInteger('length') || 3, 1, 15);
         const fps = clamp(interaction.options.getInteger('fps') || 16, 5, 24);
-        const custom_budget = clamp(interaction.options.getInteger('custom_budget') || 12_000_000, 300_000, 24_000_000);
+        const custom_budget = clamp(interaction.options.getInteger('custom_budget') || 24_000_000, 600_000, 50_000_000);
+        const model = interaction.options.getString('model') || 'wan2.1-i2v-14b-720p-Q4_K_M.gguf';
+        const shift = clamp(interaction.options.getInteger('shift') || 3, 1, 10);
 
         let workflow = JSON.parse(JSON.stringify(og_workflow))
-
-        const forceHighPrecision = prompt.includes("[FORCE_Q6]")
-        if (forceHighPrecision) {
-            prompt = prompt.replace("[FORCE_Q6]", "")
-            workflow["120"]["inputs"]["unet_name"] = "skyreels-hunyuan-I2V-Q6_K.gguf"
-        }
 
         //download the image from attachment.proxyURL
         let attachment = await loadImage(attachment_option.proxyURL,
@@ -97,16 +107,51 @@ module.exports = {
             budget = custom_budget
         }
 
-        workflow["42"]["inputs"]["noise_seed"] = Math.floor(Math.random() * 2_000_000_000)
-        workflow["110"]["inputs"]["seed"] = Math.floor(Math.random() * 2_000_000_000)
-        workflow["151"]["inputs"]["image"] = image_info.name
-        workflow["140"]["inputs"]["float"] = budget
-        workflow["153"]["inputs"]["int"] = frame_count
-        workflow["50"]["inputs"]["frame_rate"] = fps
+        if (model === 'skyreels-hunyuan-I2V-Q5_K_M.gguf') {
+            // older less optimized workflow, divide budget by 2
+            interaction.editReply({ content: "Using less optimized workflow, dividing budget by 2" });
+            budget /= 2
 
-        workflow["45"]["inputs"]["text"] = "FPS-24, " + prompt
-        if (neg_prompt !== '') {
-            workflow["45"]["inputs"]["neg_text"] = "FPS-24, " + neg_prompt
+            workflow["42"]["inputs"]["noise_seed"] = Math.floor(Math.random() * 2_000_000_000)
+            workflow["110"]["inputs"]["seed"] = Math.floor(Math.random() * 2_000_000_000)
+            workflow["151"]["inputs"]["image"] = image_info.name
+            workflow["140"]["inputs"]["float"] = budget
+            workflow["153"]["inputs"]["int"] = frame_count
+            workflow["50"]["inputs"]["frame_rate"] = fps
+
+            workflow["45"]["inputs"]["text"] = "FPS-24, " + prompt
+            if (neg_prompt !== '') {
+                workflow["45"]["inputs"]["neg_text"] = "FPS-24, " + neg_prompt
+            }
+        }
+        else {
+            workflow = JSON.parse(JSON.stringify(wan_workflow))
+
+            workflow["83"]["inputs"]["seed"] = Math.floor(Math.random() * 2_000_000_000)
+            workflow["83"]["inputs"]["nag_scale"] = neg_prompt !== '' ? 8.0 : 1.0      
+            workflow["33"]["inputs"]["image"] = image_info.name  
+            workflow["5"]["inputs"]["frame_rate"] = fps
+            workflow["18"]["inputs"]["float"] = budget
+            workflow["23"]["inputs"]["int"] = frame_count
+            workflow["14"]["inputs"]["shift"] = shift
+
+            // select model
+            workflow["10"]["inputs"]["unet_name"] = model
+            if (model === 'aniWan2114BFp8E4m3fn_i2v720p.safetensors') {
+                workflow["10"] = {
+                    "inputs": {
+                        "unet_name": "aniWan2114BFp8E4m3fn_i2v720p.safetensors",
+                        "weight_dtype": "default"
+                    },
+                    "class_type": "UNETLoader",
+                    "_meta": {
+                        "title": "Load Diffusion Model"
+                    }
+                }
+            }
+
+            workflow["81"]["inputs"]["text"] = neg_prompt
+            workflow["13"]["inputs"]["text"] = prompt;
         }
 
         ComfyClient.sendPrompt(workflow, (data) => {
@@ -125,6 +170,8 @@ module.exports = {
                 const buffer = Buffer.from(arraybuffer)
 
                 await interaction.editReply({ content: "Generation Success", files: [{ attachment: buffer, name: filename }] });
+
+                // TODO: allow user to post process the video (color matching, frame upscaling, frame interpolation)
             }).catch((err) => {
                 console.log("Failed to retrieve video", err)
                 interaction.editReply({ content: "Failed to retrieve video" });
@@ -144,7 +191,6 @@ module.exports = {
 
             interaction.editReply({ content: "Processing: " + workflow[data.node]["_meta"]["title"] + ` (${data.value}/${data.max})` });
         });
-
         // await interaction.editReply('This command is not implemented yet');
 	},
 };
--- commands/wd_txt2vid.js
+++ commands/wd_txt2vid.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const og_workflow = require('../resources/comfy_txt2vid.json')
+const wan_workflow = require('../resources/wan_txt2vid.json')
+const sf_workflow = require('../resources/wan_sf_txt2vid.json')
 const ComfyClient = require('../utils/comfy_client');
 const { get_teacache_config_from_prompt } = require('../utils/prompt_analyzer');
 const { clamp } = require('../utils/common_helper');
@@ -13,6 +15,10 @@ module.exports = {
                 option.setName('prompt')
                     .setDescription('The text prompt to generate the video')
                     .setRequired(true))
+            .addStringOption(option =>
+                option.setName('neg_prompt')
+                    .setDescription('The negative text prompt to generate the video')
+                    .setRequired(false))
             .addStringOption(option => 
                 option.setName('preset')
                     .setDescription('The preset to use for the generation')
@@ -42,34 +48,91 @@ module.exports = {
                 option.setName('portrait')
                     .setDescription('Generate a portrait video, no effect on custom preset')
                     .setRequired(false))
+            .addNumberOption(option =>
+                option.setName('seed')
+                    .setDescription('The seed for the video generation (default is random, 0 to disable)')
+                    .setRequired(false))
             .addBooleanOption(option =>
                 option.setName('no_optimize')
                     .setDescription('Do not use fast video LoRA, generation time will be doubled')
                     .setRequired(false))
-            .addBooleanOption(option =>
-                option.setName('do_upscale')
-                    .setDescription('Upscale the video to quadruple the resolution')
-                    .setRequired(false))
-            .addBooleanOption(option =>
-                option.setName('do_frame_interpolation')
-                    .setDescription('Use frame interpolation to double the frame rate')
-                    .setRequired(false))
-            .addStringOption(option => 
-                option.setName('sampler')
-                    .setDescription('The sampling method for the AI to generate art from (default is "Euler")')
-                    .addChoices(...sampler_selection))
-            .addStringOption(option => 
-                option.setName('scheduler')
-                    .setDescription('The scheduling method for the AI to generate art from (default is "Automatic")')
-                    .addChoices(...scheduler_selection))
+            // .addBooleanOption(option =>
+            //     option.setName('do_upscale')
+            //         .setDescription('Upscale the video to quadruple the resolution')
+            //         .setRequired(false))
+            // .addBooleanOption(option =>
+            //     option.setName('do_frame_interpolation')
+            //         .setDescription('Use frame interpolation to double the frame rate')
+            //         .setRequired(false))
+            // .addStringOption(option => 
+            //     option.setName('sampler')
+            //         .setDescription('The sampling method for the AI to generate art from (default is "Euler")')
+            //         .addChoices(...sampler_selection))
+            // .addStringOption(option => 
+            //     option.setName('scheduler')
+            //         .setDescription('The scheduling method for the AI to generate art from (default is "Normal")')
+            //         .addChoices(...scheduler_selection))
             .addNumberOption(option => 
                 option.setName('cfg_scale')
                     .setDescription('Lower value = more creative freedom (default is 1)'))
+            .addIntegerOption(option =>
+                option.setName('shift')
+                    .setDescription('The shift rate for the video generation (default 5, higher shift = more motion)')
+                    .setRequired(false))
+            .addStringOption(option =>
+                option.setName('model')
+                    .setDescription('The model to use for the generation')
+                    .addChoices(
+                        { name: 'Hunyuan T2V Q5_K_M - Legacy', value: 'hunyuan-video-t2v-720p-Q5_K_M.gguf' },
+                        { name: 'Wan v2.1 T2V Q8', value: 'wan2.1-t2v-14b-Q8_0.gguf' },
+                        { name: 'Wan v2.1 T2V Q4_K_S', value: 'wan2.1-t2v-14b-Q4_K_S.gguf' },
+                        { name: 'AniWan v2.1 T2V Q4_K_S', value: 'aniWan2114BFp8E4m3fn_t2v14BGGUFQ4KS.gguf' },
+                        { name: 'Wan v2.1 T2V Self-forcing - Fast', value: 'self_forcing_dmd.pt' }
+                    ))
 
     ,
 
+    get_video_dimension(preset, custom_width, custom_height, custom_length, custom_fps, portrait) {
+        let width = 512;
+        let height = 320;
+        let length = 51;
+        let fps = 16;
+
+        if (preset === 'fast') {
+            width = 512;
+            height = 320;
+            length = 3 * 16; // 3 seconds
+            fps = 16;
+        } else if (preset === 'sd') {
+            width = 640;
+            height = 480;
+            length = 3 * 16; // 3 seconds
+            fps = 16;
+        } else if (preset === 'long') {
+            width = 512;
+            height = 320;
+            length = 5 * 16; // 5 seconds
+            fps = 16;
+        } else if (preset === 'custom') {
+            width = custom_width || width;
+            height = custom_height || height;
+            fps = custom_fps || fps;
+            length = custom_length * fps || length; // custom length in seconds
+        }
+
+        if (portrait) {
+            // swap width and height
+            [width, height] = [height, width];
+        }
+
+        return { width, height, length, fps };
+    },
+
 	async execute(interaction, client) {
+        await interaction.deferReply();
+
         let prompt = interaction.options.getString('prompt');
+        let neg_prompt = interaction.options.getString('neg_prompt') || '';
         const preset = interaction.options.getString('preset') || 'fast';
         const custom_width = clamp(interaction.options.getInteger('custom_width') || 512, 64, 1280);
         const custom_height = clamp(interaction.options.getInteger('custom_height') || 320, 64, 1280);
@@ -79,36 +142,21 @@ module.exports = {
         const no_optimize = interaction.options.getBoolean('no_optimize') || false;
         const do_upscale = interaction.options.getBoolean('do_upscale') || false;
         const do_frame_interpolation = interaction.options.getBoolean('do_frame_interpolation') || false;
+        const seed = interaction.options.getNumber('seed') || 0
         const sampler = interaction.options.getString('sampler') || 'Euler';
-        const scheduler = interaction.options.getString('scheduler') || 'Automatic'
+        const scheduler = interaction.options.getString('scheduler') || 'Normal'
         const cfg_scale = clamp(interaction.options.getNumber('cfg_scale') || 1, 1, 10);
+        const model = interaction.options.getString('model') || 'wan2.1-t2v-14b-Q4_K_S.gguf';
+        const shift = clamp(interaction.options.getInteger('shift') || 5, 1, 10);
 
         let workflow = JSON.parse(JSON.stringify(og_workflow))
 
-        const forceHighPrecision = prompt.includes("[FORCE_Q6]")
-        if (forceHighPrecision) {
-            prompt = prompt.replace("[FORCE_Q6]", "")
-            workflow["11"]["inputs"]["unet_name"] = "hunyuan-video-t2v-720p-Q6_K.gguf"
-        }
+        let dimensions = this.get_video_dimension(preset, custom_width, custom_height, custom_length, custom_fps, portrait);
+        const budget = dimensions.width * dimensions.height * dimensions.length;
 
-        if (preset == 'fast') {
-            workflow["12"]["inputs"]["width"] = 512
-            workflow["12"]["inputs"]["height"] = 320
-            workflow["12"]["inputs"]["length"] = 49
-        }
-        else if (preset == 'sd') {
-            workflow["12"]["inputs"]["width"] = 640
-            workflow["12"]["inputs"]["height"] = 480
-            workflow["12"]["inputs"]["length"] = 49
-        }
-        else if (preset == 'long') {
-            workflow["12"]["inputs"]["width"] = 512
-            workflow["12"]["inputs"]["height"] = 320
-            workflow["12"]["inputs"]["length"] = 81
-        }
-        else {
-            // calculate budget
-            const budget = custom_width * custom_height * custom_fps * custom_length
+        if (model === 'hunyuan-video-t2v-720p-Q5_K_M.gguf') {
+            interaction.channel.send({ content: "neg_prompt, seed, shift will be ignored for this workflow" });
+
             if (budget > 24_000_000) {
                 interaction.editReply({ content: "Budget exceeded 24M pixels! My PC cannot handle this xd" });
                 return
@@ -117,50 +165,96 @@ module.exports = {
                 interaction.channel.send({ content: "Budget exceeded 16M pixels, generation may take quite a while" });
             }
 
-            workflow["12"]["inputs"]["width"] = custom_width
-            workflow["12"]["inputs"]["height"] = custom_height
-            workflow["12"]["inputs"]["length"] = custom_length * custom_fps
-            workflow["41"]["inputs"]["frame_rate"] = custom_fps
+            workflow["12"]["inputs"]["width"] = dimensions.width;
+            workflow["12"]["inputs"]["height"] = dimensions.height;
+            workflow["12"]["inputs"]["length"] = dimensions.length;
+            workflow["41"]["inputs"]["frame_rate"] = dimensions.fps;
+
+            if (no_optimize) {
+                delete workflow["51"]
+                workflow["8"]["inputs"]["model"] = ["45", 0]
+                workflow["8"]["inputs"]["steps"] = 30
+            }
+
+            if (!do_upscale) {
+                delete workflow["55"]
+                delete workflow["56"]
+                workflow["46"]["inputs"]["frames"] = ["37", 0]
+            }
+
+            if (do_frame_interpolation) {
+                workflow["47"]["inputs"]["Value"] = 2
+                workflow["41"]["inputs"]["frame_rate"] = preset == 'custom' ? custom_fps * 2 : 32
+            }
+
+            await interaction.deferReply();
+
+            // check if user want to use teacache
+            const teacache_check = get_teacache_config_from_prompt(prompt)
+            if (teacache_check.teacache_config) {
+                workflow["45"]["inputs"]["rel_l1_thresh"] = teacache_check.teacache_config.threshold
+            }
+            prompt = teacache_check.prompt
+
+            workflow["15"]["inputs"]["text"] = prompt
+
+            workflow["8"]["inputs"]["sampler_name"] = sampler_to_comfy_name_mapping[sampler] ?? "euler"
+            workflow["8"]["inputs"]["scheduler"] = scheduler_to_comfy_name_mapping[scheduler] ?? "normal"
+            workflow["8"]["inputs"]["cfg"] = cfg_scale
         }
+        else if (model === 'self_forcing_dmd.pt') {
+            // self-forcing workflow
+            workflow = JSON.parse(JSON.stringify(sf_workflow))
+            interaction.channel.send({ content: "cfg_scale, neg_prompt, sampler, scheduler will be ignored for this workflow" });
 
-        if (portrait && preset != 'custom') {
-            // swap width and height
-            var a = workflow["12"]["inputs"]["width"]
-            workflow["12"]["inputs"]["width"] = workflow["12"]["inputs"]["height"]
-            workflow["12"]["inputs"]["height"] = a
+            if (budget > 80_000_000) {
+                interaction.editReply({ content: "Budget exceeded 80M pixels! My PC cannot handle this xd" });
+                return
+            }
+            else if (budget > 50_000_000) {
+                interaction.channel.send({ content: "Budget exceeded 50M pixels, generation may take quite a while" });
+            }
+
+            workflow["40"]["inputs"]["width"] = dimensions.width;
+            workflow["40"]["inputs"]["height"] = dimensions.height;
+            workflow["40"]["inputs"]["length"] = dimensions.length;
+            workflow["49"]["inputs"]["frame_rate"] = dimensions.fps;
+
+            workflow["48"]["inputs"]["shift"] = shift;
+            workflow["3"]["inputs"]["seed"] = seed > 0 ? seed : Math.floor(Math.random() * 2_000_000_000);
+            workflow["6"]["inputs"]["text"] = prompt;
         }
+        else {
+            // wan workflow
+            workflow = JSON.parse(JSON.stringify(wan_workflow))
+            interaction.channel.send({ content: "cfg_scale, sampler, scheduler will be ignored for this workflow" });
 
-        if (no_optimize) {
-            delete workflow["51"]
-            workflow["8"]["inputs"]["model"] = ["45", 0]
-            workflow["8"]["inputs"]["steps"] = 30
+            if (budget > 50_000_000) {
+                interaction.editReply({ content: "Budget exceeded 50M pixels! My PC cannot handle this xd" });
+                return
+            }
+            else if (budget > 36_000_000) {
+                interaction.channel.send({ content: "Budget exceeded 36M pixels, generation may take quite a while" });
+            }
+
+            workflow["40"]["inputs"]["width"] = dimensions.width;
+            workflow["40"]["inputs"]["height"] = dimensions.height;
+            workflow["40"]["inputs"]["length"] = dimensions.length;
+            workflow["30"]["inputs"]["frame_rate"] = dimensions.fps;
+
+            workflow["48"]["inputs"]["shift"] = shift;
+
+            /// prompt
+            workflow["6"]["inputs"]["text"] = prompt;
+            workflow["7"]["inputs"]["text"] = neg_prompt;
+
+            // model selection
+            workflow["291"]["inputs"]["unet_name"] = model;
+
+            // sampler
+            workflow["295"]["inputs"]["seed"] = seed > 0 ? seed : Math.floor(Math.random() * 2_000_000_000);
+            workflow["295"]["inputs"]["nag_scale"] = neg_prompt !== '' ? 8.0 : 1.0;
         }
-
-        if (!do_upscale) {
-            delete workflow["55"]
-            delete workflow["56"]
-            workflow["46"]["inputs"]["frames"] = ["37", 0]
-        }
-
-        if (do_frame_interpolation) {
-            workflow["47"]["inputs"]["Value"] = 2
-            workflow["41"]["inputs"]["frame_rate"] = preset == 'custom' ? custom_fps * 2 : 32
-        }
-
-        await interaction.deferReply();
-
-        // check if user want to use teacache
-        const teacache_check = get_teacache_config_from_prompt(prompt)
-        if (teacache_check.teacache_config) {
-            workflow["45"]["inputs"]["rel_l1_thresh"] = teacache_check.teacache_config.threshold
-        }
-        prompt = teacache_check.prompt
-
-        workflow["15"]["inputs"]["text"] = prompt
-
-        workflow["8"]["inputs"]["sampler_name"] = sampler_to_comfy_name_mapping[sampler] ?? "euler"
-        workflow["8"]["inputs"]["scheduler"] = scheduler_to_comfy_name_mapping[scheduler] ?? "normal"
-        workflow["8"]["inputs"]["cfg"] = cfg_scale
 
         ComfyClient.sendPrompt(workflow, (data) => {
             if (data.node !== null) interaction.editReply({ content: "Processing: " + workflow[data.node]["_meta"]["title"] });
