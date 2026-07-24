1d99e167b9376fb0528f51121f0d7b20363d7f94
neroyuki <kingrfminecraft@gmail.com>
2026-07-25 02:24:50 +0700

    address update

--- utils/ai_server_config.js
+++ utils/ai_server_config.js
@@ -7,7 +7,7 @@ const { convert_upload_path_to_file_data } = require('./common_helper');
 const server_pool = [
     {
         index: 0,
-        url: process.env.BOT_ENV === 'lan' ? 'http://192.168.1.7:7860' : 'http://192.168.196.142:7860',
+        url: process.env.BOT_ENV === 'lan' ? 'http://192.168.1.5:7860' : 'http://192.168.196.142:7860',
         fn_index_create: 616,
         fn_index_abort: 51,
         fn_index_img2img: 1330,
